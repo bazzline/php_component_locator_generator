7edef802b5f27038d2c860020d80b8e288b69dc6
stevleibelt <stev@leibelt.de>
2014-12-29 13:26:58 +0100

    moved to AbstractInterfaceGenerator

--- source/Net/Bazzline/Component/Locator/FactoryInterfaceGenerator.php
+++ source/Net/Bazzline/Component/Locator/FactoryInterfaceGenerator.php
@@ -17,112 +17,17 @@ use Net\Bazzline\Component\CodeGenerator\FileGenerator;
  * Class FactoryInterfaceGenerator
  * @package Net\Bazzline\Component\Locator
  */
-class FactoryInterfaceGenerator extends AbstractGenerator
+class FactoryInterfaceGenerator extends AbstractInterfaceGenerator
 {
-    /**
-     * @var ClassGeneratorFactory
-     */
-    private $classGeneratorFactory;
-
-    /**
-     * @var DocumentationGeneratorFactory
-     */
-    private $documentationGeneratorFactory;
-
-    /**
-     * @var FileGeneratorFactory
-     */
-    private $fileGeneratorFactory;
-
-    /**
-     * @var MethodGeneratorFactory
-     */
-    private $methodGeneratorFactory;
-
-    /**
-     * @param \Net\Bazzline\Component\CodeGenerator\Factory\ClassGeneratorFactory $classGeneratorFactory
-     * @return $this
-     */
-    public function setClassGeneratorFactory(ClassGeneratorFactory $classGeneratorFactory)
-    {
-        $this->classGeneratorFactory = $classGeneratorFactory;
-
-        return $this;
-    }
-
-    /**
-     * @param \Net\Bazzline\Component\CodeGenerator\Factory\DocumentationGeneratorFactory $documentationGeneratorFactory
-     * @return $this
-     */
-    public function setDocumentationGeneratorFactory(DocumentationGeneratorFactory $documentationGeneratorFactory)
-    {
-        $this->documentationGeneratorFactory = $documentationGeneratorFactory;
-
-        return $this;
-    }
-
-    /**
-     * @param \Net\Bazzline\Component\CodeGenerator\Factory\FileGeneratorFactory $fileGeneratorFactory
-     * @return $this
-     */
-    public function setFileGeneratorFactory(FileGeneratorFactory $fileGeneratorFactory)
-    {
-        $this->fileGeneratorFactory = $fileGeneratorFactory;
-
-        return $this;
-    }
-
-    /**
-     * @param \Net\Bazzline\Component\CodeGenerator\Factory\MethodGeneratorFactory $methodGeneratorFactory
-     * @return $this
-     */
-    public function setMethodGeneratorFactory(MethodGeneratorFactory $methodGeneratorFactory)
-    {
-        $this->methodGeneratorFactory = $methodGeneratorFactory;
-
-        return $this;
-    }
-
     /**
      * @throws RuntimeException
      */
     public function generate()
     {
-        $fileName = 'FactoryInterface.php';
-        $this->moveOldFileIfExists($this->configuration->getFilePath(), $fileName);
-
-        $fileGenerator = $this->createFile($this->fileGeneratorFactory->create());
-        $classGenerator = $this->createClass(
-            $this->classGeneratorFactory->create(),
-            $this->configuration,
-            $this->documentationGeneratorFactory,
-            $this->methodGeneratorFactory
+        $this->generateInterface(
+            'FactoryInterface'.
+            $this->configuration->getFileNameExtension()
         );
-
-        $fileGenerator->addClass($classGenerator);
-        $fileContent = $fileGenerator->generate();
-
-        $fullQualifiedPathName = $this->configuration->getFilePath() .
-            DIRECTORY_SEPARATOR . $fileName;
-        $this->dumpToFile($fullQualifiedPathName, $fileContent);
-    }
-
-    /**
-     * @param FileGenerator $fileGenerator
-     * @return FileGenerator
-     */
-    private function createFile(FileGenerator $fileGenerator)
-    {
-        $fileGenerator->addFileContent(
-            array(
-                '/**',
-                ' * @author Net\Bazzline\Component\Locator',
-                ' * @since ' . date('Y-m-d'),
-                ' */'
-            )
-        );
-
-        return $fileGenerator;
     }
 
     /**
@@ -132,7 +37,7 @@ class FactoryInterfaceGenerator extends AbstractGenerator
      * @param MethodGeneratorFactory $methodGeneratorFactory
      * @return ClassGenerator
      */
-    private function createClass(ClassGenerator $classGenerator, Configuration $configuration, DocumentationGeneratorFactory $documentationGeneratorFactory, MethodGeneratorFactory $methodGeneratorFactory)
+    protected function createInterface(ClassGenerator $classGenerator, Configuration $configuration, DocumentationGeneratorFactory $documentationGeneratorFactory, MethodGeneratorFactory $methodGeneratorFactory)
     {
         $classGenerator->markAsInterface();
         $classGenerator->setDocumentation($documentationGeneratorFactory->create());
